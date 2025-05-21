a8691619afd382d6d25718de54f151b65529185a
João Victor Lemos <79033128+Lemos0250@users.noreply.github.com>
2025-05-21 20:01:52 -0300

    rotate

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const parallax_el = document.querySelectorAll(".parallax");
 let xValue = 0, 
     yValue = 0;
 
+    let rotateDegree = 0;
+
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
     console.log(xValue, yValue);
 
+    rotateDegree = (xValue / (window.innerWidth /  2)) * 20;
+
+    console.log(rotateDegree);
+
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
+        let speedz = el.dataset.speedz;
+        let rotateSpeed = el.dataset.rotation;
+
+        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+        let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
+        console.log(zValue);
 
         el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px))
-                              translateY(calc(-50% + ${yValue * speedy}px))`;
+                              rotateY(${rotateDegree * rotateSpeed}deg)
+                              translateY(calc(-50% + ${yValue * speedy}px))
+                              perspective(2300px)
+                              translateZ(${zValue * speedz}px)`;
     });
 });
