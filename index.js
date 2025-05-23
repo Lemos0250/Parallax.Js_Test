ac012e07cec073a74e07d425f452a4f52d476c93
João Victor Lemos <79033128+Lemos0250@users.noreply.github.com>
2025-05-23 18:28:12 -0300

    Responsive 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const parallax_el = document.querySelectorAll(".parallax");
+const main = document.querySelector("main")
 
 let xValue = 0, 
     yValue = 0;
@@ -39,6 +40,12 @@ window.addEventListener("mousemove", (e) => {
 
     update(e.clientX);
 });
+
+if(window.innerWidth >= 725) {
+    main.style.maxHeight = `${window.innerWidth * 0.6}px`;
+} else {
+    main.style.maxHeight = `${window.innerWidth * 1.6}px`
+}
 
 /*Testing GSAP Library Animation*/ 
 
